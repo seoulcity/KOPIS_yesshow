61236998d84bc3dce156869ac94b6e2ff2be0b27
Kim JungHyun <lallacri86@gmail.com>
2024-05-30 23:36:28 +0000

    Update packages and the static directory

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
+
+const rootDir = process.cwd();
 
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    fs: {
+      allow: [
+        rootDir,
+        path.join(rootDir, 'src'),
+        path.join(rootDir, '.svelte-kit'),
+        path.join(rootDir, 'node_modules'),
+      ]
+    }
+  }
 });
