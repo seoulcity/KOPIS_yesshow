11cdad8613aedbcc29bee5b6f56fc27c1fe1bc39
Kim JungHyun <lallacri86@gmail.com>
2024-08-10 09:25:16 +0000

    Make accessible when operating frontend server

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
@@ -7,6 +8,8 @@ const rootDir = process.cwd();
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
+    host: '0.0.0.0',  // 모든 네트워크 인터페이스에서 접근 허용
+    port: 8080,       // GCP에서 일반적으로 사용하는 포트
     fs: {
       allow: [
         rootDir,
@@ -14,6 +17,7 @@ export default defineConfig({
         path.join(rootDir, '.svelte-kit'),
         path.join(rootDir, 'node_modules'),
       ]
-    }
-  }
+    },
+    cors: true,  // 모든 출처에서의 CORS 요청 허용
+  },
 });
